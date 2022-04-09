7294d30e9cb22cd6d1784c996f0f1271a5f472ed
georgios63 <georgemilitsopoulos63@gmail.com>
2022-04-09 16:22:01 +0200

    creating the post route for new stories

--- routers/homePage.js
+++ routers/homePage.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const Spaces = require("../models").space;
 const User = require("../models").user;
 const Stories = require("../models").story;
+const authMiddleWare = require("../auth/middleware");
 
 const router = new Router();
 
@@ -15,16 +16,15 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/mySpace", authMiddleWare, async (req, res) => {
+  const current_user = req.user.id;
   try {
-    const userId = req.params.id;
+    // const spaceId = req.params.id;
     const spaceById = await Spaces.findOne({
-      where: { userId },
+      where: { userId: current_user },
       include: [
         {
           model: Stories,
-          where: { id: userId },
-          right: true,
         },
       ],
     });
@@ -34,6 +34,69 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.get("/mySpace/myStories", authMiddleWare, async (req, res, next) => {
+  const current_user = req.user.id;
+  try {
+    const storyById = await Stories.findAll({
+      where: { spaceId: current_user },
+    });
+    res.send(storyById);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.post("/mySpace", authMiddleWare, async (req, res) => {
+  try {
+    const story = req.body;
+    const newStory = await Stories.create({
+      name: story.name,
+      content: story.content,
+      imageUrl: story.imageUrl,
+    });
+
+    res.send(newStory);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/mySpace", authMiddleWare, async (req, res) => {
+  try {
+    const id = req.body.id;
+    await Stories.destroy({
+      where: { id },
+    }).then(() => {
+      res.status(204).end();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// router.get("/userSpace/:userId", async (req, res) => {
+//   const current_user = req.params.userId
+// })
+
+router.get("/:id", async (req, res) => {
+  try {
+    const spaceId = req.params.id;
+    const spaceById = await Spaces.findOne({
+      where: { id: spaceId },
+      include: [
+        {
+          model: Stories,
+        },
+      ],
+    });
+    res.send(spaceById);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// not needed the one above does this already
 router.get("/stories/:id", async (req, res) => {
   try {
     const spaceId = req.params.id;
@@ -47,7 +110,6 @@ router.get("/stories/:id", async (req, res) => {
         },
       ],
     });
-
     res.send(storiesById);
   } catch (error) {
     console.log(error);
